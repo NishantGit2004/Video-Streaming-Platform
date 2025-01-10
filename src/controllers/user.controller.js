c300d2fe773b2aa89a33ac0b2b4d84bb93a4143b
Nishant Chopde <nishantchopde2004@gmail.com>
2025-01-10 21:19:22 +0530

    Added comment, like, playlist, tweet models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -130,8 +130,8 @@ const logoutUser = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         req.user.id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1 // This removes the field from the document
             }
         },
         {
